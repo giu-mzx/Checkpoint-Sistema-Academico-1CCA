0f8286d762ac064ef37c2a0cd0a1422390904146
Mikaella Lucindo <mikaellasantos092@gmail.com>
2026-04-27 15:50:39 -0300

    Estrutura do menu

--- src/main.c
+++ src/main.c
@@ -1,8 +1,24 @@
 #include <stdio.h>
- 
-int main() {
+
+int main()
+{
     int opcao;
     float n1 = 0, n2 = 0, media = 0;
     int notasInseridas = 0;
     int mediaCalculada = 0;
+
+    do
+    {
+        printf("\n============================\n");
+        printf(" SISTEMA EQUIPE\n");
+        printf("============================\n");
+        printf("1 - Inserir notas\n");
+        printf("2 - Calcular media\n");
+        printf("3 - Verificar situacao\n");
+        printf("4 - Exibir resultado\n");
+        printf("5 - Calcular derivada\n");
+        printf("6 - Sair\n");
+        printf("Escolha uma opcao: ");
+        scanf("%d", &opcao);
+    } while (opcao != 6);
 }
